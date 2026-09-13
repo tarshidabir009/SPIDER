cb0f7a67573a441fb43aba9b669072ae642b89e9
tarshidabir009 <abirdcacc2020@gmail.com>
2026-09-13 10:21:15 +0300

    Update import path for Context module

--- CodeX.py
+++ CodeX.py
@@ -27,7 +27,7 @@ import discord
 from discord import Spotify
 from discord.ext import commands, tasks
 
-from core import Context
+from bot.core import Context
 from core.Cog import Cog
 from core.zyrox import zyrox
 from utils.Tools import *
